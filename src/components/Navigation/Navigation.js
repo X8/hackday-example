33c1e3e15dbaaf8347521b9723bd6570ceea13a0
Michal Lomnicki <michal.lomnicki@gmail.com>
2015-04-15 16:46:44 +0200

    Add a link to the API

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,6 +12,7 @@ class Navigation extends Component {
         <Nav>
           <NavItem href='http://footballhackday.github.io/london/'>Website</NavItem>
           <NavItem href='https://github.com/X8/hackday-example'>Source code</NavItem>
+          <NavItem href='https://hackday-api.herokuapp.com/'>API</NavItem>
         </Nav>
       </Navbar>
     )
